71c9acea1516c989771340158965ce6fcbd32649
Alitsu <gregorycarvalho3@gmail.com>
2023-06-11 21:14:15 +0200

    Misé à jour maybe rollback

--- APP_FILMS_164/genres/gestion_genres_crud.py
+++ APP_FILMS_164/genres/gestion_genres_crud.py
@@ -254,7 +254,7 @@ def genre_delete_wtf():
                 valeur_delete_dictionnaire = {"value_id_genre": id_genre_delete}
                 print("valeur_delete_dictionnaire ", valeur_delete_dictionnaire)
 
-                str_sql_delete_films_genre = """DELETE FROM t_tache WHERE fk_installation = %(value_id_genre)s"""
+                str_sql_delete_films_genre = """DELETE FROM t_client WHERE fk_installation = %(value_id_genre)s"""
                 str_sql_delete_idgenre = """DELETE FROM t_client WHERE id_client = %(value_id_genre)s"""
                 # Manière brutale d'effacer d'abord la "fk_genre", même si elle n'existe pas dans la "t_client"
                 # Ensuite on peut effacer le genre vu qu'il n'est plus "lié" (INNODB) dans la "t_client"
